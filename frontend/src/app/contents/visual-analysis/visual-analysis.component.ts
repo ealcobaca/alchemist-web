9e9a33de8a3cb85c18d3e919354df4037c9c1e7f
ealcobaca <e.alcobaca@gmail.com>
2019-01-29 15:12:50 -0200

    start

---
+++ frontend/src/app/contents/visual-analysis/visual-analysis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visual-analysis',
+  templateUrl: './visual-analysis.component.html',
+  styleUrls: ['./visual-analysis.component.scss']
+})
+export class VisualAnalysisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
